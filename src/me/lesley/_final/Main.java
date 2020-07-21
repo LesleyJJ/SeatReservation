09cee90f9053e9e560ec3db8453469ec59479102
Lesley <jacobjohnlesley@gmail.com>
2020-07-22 00:40:25 +0530

    Initial commit

---
+++ src/me/lesley/_final/Main.java
@@ -0,0 +1,22 @@
+package me.lesley._final;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("MC", 8, 12);
+        theatre.getSeats();
+
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay: ");
+        }else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay: ");
+        }else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+    }
+}
